d4b3b1c73e9766efefcb338a48ced60b9cb43f98
Estel Smith <estel.smith@gmail.com>
2014-12-06 14:06:14 -0500

    Created an application class to handle the request/response cycle.

---
+++ src/App/Web/Application.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Web;
+
+use App\Web\Action\ActionInterface;
+use DCP\Di\Container;
+use DCP\Di\ContainerAwareInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+
+class Application implements ContainerAwareInterface
+{
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @var UrlMatcherInterface
+     */
+    private $urlMatcher;
+
+    /**
+     * @var Container
+     */
+    private $container;
+
+    public function __construct(Request $request, UrlMatcherInterface $urlMatcher)
+    {
+        $this->request = $request;
+        $this->urlMatcher = $urlMatcher;
+    }
+
+    /**
+     * @return Response
+     */
+    public function run()
+    {
+        $request = $this->request;
+        $urlMatcher = $this->urlMatcher;
+
+        try {
+            $route = $urlMatcher->match($request->getRequestUri());
+            $actionName = sprintf('App\Web\Action\%sAction', $route['controller']);
+
+            /** @var ActionInterface $action */
+            $action = $this->container->get($actionName);
+            $responder = $action->execute();
+
+            return $responder->execute();
+        } catch(ResourceNotFoundException $e) {
+            return new Response(sprintf(':-( Could not find %s', $request->getRequestUri()), Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    public function setContainer(Container $container)
+    {
+        $this->container = $container;
+    }
+}
--- web/app.php
+++ web/app.php
@@ -3,31 +3,8 @@
 require '../vendor/autoload.php';
 require '../config/config.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
-use App\Web\Action\ActionInterface;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\HttpFoundation\Response;
+use App\Web\Application;
 
-/** @var Request $request */
-$request = $di->get('request');
-
-/** @var UrlMatcherInterface $matcher */
-$matcher = $di->get('url_matcher');
-
-try {
-    // @TODO: Create an application class to handle this.
-    $route = $matcher->match($request->getRequestUri());
-    $action = sprintf('App\Web\Action\%sAction', $route['controller']);
-
-    /** @var ActionInterface $controller */
-    $controller = $di->get($action);
-
-    $responder = $controller->execute();
-    $response = $responder->execute();
-    $response->send();
-} catch (ResourceNotFoundException $e) {
-    $content = sprintf(':-( could not find %s', $request->getRequestUri());
-    $response = new Response($content, Response::HTTP_NOT_FOUND);
-    $response->send();
-}
+/** @var Application $app */
+$app = $di->get(Application::class);
+$app->run()->send();
