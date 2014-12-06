36d211a318992bd5337fdc2c3aca7e9bed8b24f9
Estel Smith <estel.smith@gmail.com>
2014-12-06 15:03:10 -0500

    Enabled error reporting if running from localhost.

--- config/config.php
+++ config/config.php
@@ -2,3 +2,12 @@
 
 require 'di.php';
 require 'routing.php';
+
+switch ($_SERVER['SERVER_NAME']) {
+    case 'localhost':
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+        break;
+    default:
+        break;
+}
