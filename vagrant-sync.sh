c03b1cceb9a125de5de9104628a27b04b1029c7a
Estel Smith <estel.smith@gmail.com>
2014-12-06 13:09:20 -0500

    Updated composer, updated vagrant-sync script.

--- vagrant-sync.sh
+++ vagrant-sync.sh
@@ -4,7 +4,7 @@ echo 'Removing /var/www/html/*'
 sudo rm -rf /var/www/html/*
 
 echo 'Copying application files'
-sudo cp -R /vagrant/* /var/www/html
+sudo cp -RT /vagrant /var/www/html
 
 echo 'Updating permissions'
 sudo chown -R vagrant:vagrant /var/www/html
