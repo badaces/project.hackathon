7d6843a2662b6b5e526270f2e40d49b7f0667682
Cameron Condry <ccondry@pltw.org>
2014-12-06 15:19:43 -0500

    .

---
+++ templates/Base/javascript.php
@@ -0,0 +1,4 @@
+<script src="<?= $this->asset('js/jquery.min.js') ?>"></script>
+<script src="<?= $this->asset('js/eventlib.js') ?>"></script>
+<script src="<?= $this->asset('js/helpme.js') ?>"></script>
+<script src="<?= $this->asset('js/d3.min.js') ?>"></script>
