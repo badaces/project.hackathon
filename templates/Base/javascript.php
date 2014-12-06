f9f60befb668d7c5c2728d39dc2e1d568da3e53a
Cameron Condry <ccondry@pltw.org>
2014-12-06 15:22:53 -0500

    Updated javascript includes

--- templates/Base/javascript.php
+++ templates/Base/javascript.php
@@ -1,4 +1,10 @@
-<script src="<?= $this->asset('js/jquery.min.js') ?>"></script>
-<script src="<?= $this->asset('js/eventlib.js') ?>"></script>
-<script src="<?= $this->asset('js/helpme.js') ?>"></script>
-<script src="<?= $this->asset('js/d3.min.js') ?>"></script>
+<?php // Libraries ?>
+
+<script src="<?= $this->asset('js/vendor/jquery/jquery.min.js') ?>"></script>
+<script src="<?= $this->asset('js/vendor/eventlib/eventlib.js') ?>"></script>
+<script src="<?= $this->asset('js/vendor/helpme/helpme.js') ?>"></script>
+<script src="<?= $this->asset('js/vendor/d3/d3.min.js') ?>"></script>
+
+<?php // Application Specific ?>
+
+<script src="<?= $this->asset('js/main.js') ?>"></script>
