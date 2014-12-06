b0396784aafd5b63fbf45232ae5264d41b9cda2b
Estel Smith <estel.smith@gmail.com>
2014-12-06 11:08:25 -0500

    Updating initial support files.

--- vagrant-sync.sh
+++ vagrant-sync.sh
@@ -8,3 +8,6 @@ sudo cp -R /vagrant/* /var/www/html
 
 echo 'Updating permissions'
 sudo chown -R vagrant:vagrant /var/www/html
+
+echo 'Ensuring vagrant-sync is executable'
+sudo chmod +x vagrant-sync.sh
