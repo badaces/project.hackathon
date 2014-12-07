67d58b63d9b32ef213a6edf45aa563994594e141
Aime Menendez <aime.menendez@gmail.com>
2014-12-07 14:13:30 -0500

    SVG folder changes, home.php changes

--- src/templates/Base/default.php
+++ src/templates/Base/default.php
@@ -7,9 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-
-    <link rel="stylesheet" href="<?= $this->asset('css/main.css') ?>"/>
-
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700|Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="<?= $this->asset('css/main.css') ?>"/>
     <title>Globathon - <?= $title ?></title>
 </head>
 <body>
--- src/templates/Home/home.php
+++ src/templates/Home/home.php
@@ -1,6 +1,42 @@
 <?php $this->layout('Base/default', ['title' => 'Home']) ?>
 	
-<h1>Welcome to the end of the world!</h1>
-<div class="canvas"></div>
+<h1>Application Name</h1>
+
+<p>Application description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit quis explicabo, at totam tempora cumque, qui quaerat praesentium, quod voluptatem tempore aliquam dicta nisi blanditiis debitis soluta, eveniet dolor cum eligendi quas. Expedita nam nulla molestiae corrupti sunt sit earum, numquam temporibus. Repellat rerum omnis repudiandae hic placeat, eligendi impedit, reiciendis quos recusandae eveniet quam deleniti cupiditate perspiciatis, iusto quisquam.</p>
+
+<div id="main-canvas">
+	
+	<div class="stage-wrapper">
+		
+		<div class="infographic">
+			<img src="/web/svg/stage_CO2pollution.svg" width="100%" height="auto" alt="Bla bla bla">
+		</div>
+
+		<div class="associated-data"></div>
+	</div>
+
+</div>
+
+<div id="information-wrapper">
+
+	<div class="info-injector">
+		<h2>Greenhouse Effect</h2>
+		<p>Introduction and description of the subject lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi beatae magnam, blanditiis, quae sint aspernatur voluptatum veniam unde corporis vero harum. Inventore facere vel repellendus mollitia laboriosam quis expedita voluptate voluptatem repudiandae, hic quia porro, ab odio aspernatur, a? Alias!</p>
+		<p class="source-caption">Wikipedia</p>
+	</div>
+
+	<div class="additional-data"></div>
+
+	<div class="additional-data twitter-feed"></div>
+</div>
 
 
+<div id="main-canvas-selectors">
+	<ul>
+		<li>Cryosphere</li>
+		<li>Hydrosphere</li>
+		<li>Lithosphere</li>
+		<li>Biosphere</li>
+		<li>Atmosphere</li>
+	</ul>
+</div>
