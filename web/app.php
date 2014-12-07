a4f000cbecd2efc2bcdec949ad52cac5c7efc41d
Estel Smith <estel.smith@gmail.com>
2014-12-07 17:16:45 -0500

    Reverted my accidental committing of changes to app.php

--- web/app.php
+++ web/app.php
@@ -4,13 +4,7 @@ require '../vendor/autoload.php';
 require '../src/config/config.php';
 
 use App\Web\Application;
-use App\Web\API\Consumer\Wikipedia;
 
-///** @var Application $app */
-//$app = $di->get(Application::class);
-//$app->run()->send();
-
-/** @var Wikipedia $api */
-$api = $di->get(Wikipedia::class);
-
-$api->getArticleByName('Greenhouse effect');
+/** @var Application $app */
+$app = $di->get(Application::class);
+$app->run()->send();
