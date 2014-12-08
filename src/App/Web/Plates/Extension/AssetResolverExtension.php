45a1ba5c0232bf0b102b5ad19198e19162a1695b
Estel Smith <estel.smith@gmail.com>
2014-12-08 01:33:53 -0500

    Adding asset tracking update for production server.

--- src/App/Web/Plates/Extension/AssetResolverExtension.php
+++ src/App/Web/Plates/Extension/AssetResolverExtension.php
@@ -2,11 +2,22 @@
 
 namespace App\Web\Plates\Extension;
 
+use CBC\Utility\Configuration;
 use League\Plates\Engine;
 use League\Plates\Extension\ExtensionInterface;
 
 class AssetResolverExtension implements ExtensionInterface
 {
+    /**
+     * @var bool
+     */
+    private $isLocal;
+
+    public function __construct(Configuration $configuration)
+    {
+        $this->isLocal = $configuration->get('app.local');
+    }
+
     public function register(Engine $engine)
     {
         $engine->registerFunction('asset', [$this, 'asset']);
@@ -15,6 +26,11 @@ class AssetResolverExtension implements ExtensionInterface
     public function asset($path)
     {
         $actualPath = $path;
+        $newPath = '/web/%s';
+
+        if ($this->isLocal) {
+            $newPath = '/%s';
+        }
 
         $hasLeadingSlash = strpos($path, '/') === 0;
 
--- src/config/config.php
+++ src/config/config.php
@@ -1,15 +1,24 @@
 <?php
 
+use CBC\Utility\Configuration;
+
 mb_internal_encoding('UTF-8');
 
 require 'di.php';
 require 'routing.php';
 
+/** @var Configuration $config */
+$config = $di->get('config');
+$isLocal = false;
+
 switch ($_SERVER['SERVER_NAME']) {
     case 'localhost':
         error_reporting(E_ALL);
         ini_set('display_errors', 1);
+        $isLocal = true;
         break;
     default:
         break;
 }
+
+$config->set('app.local', $isLocal);
